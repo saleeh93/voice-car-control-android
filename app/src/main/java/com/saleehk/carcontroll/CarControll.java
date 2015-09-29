e77aa9196d90ca3dee73948f9b0237c75390f694
Saleeh <saleeh93@gmail.com>
2015-09-29 11:33:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/saleehk/carcontroll/CarControll.java
@@ -0,0 +1,25 @@
+package com.saleehk.carcontroll;
+
+/**
+ * Created by Appilder on 9/29/2015.
+ */
+public class CarControll {
+    public void moveForward() {
+
+    }
+    public void moveBackward() {
+
+    }
+    public void turnLeft() {
+
+    }
+    public void turnRight() {
+
+    }
+    public void stopCar() {
+
+    }
+    public void openDoor() {
+
+    }
+}
